f3c07ea87a9513a4f06aec306b74cbf131a31dab
Alberto Bollino <alberto.bollino@outlook.it>
2022-06-06 18:13:29 +0200

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,8 +65,8 @@ export default {
   ],
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:3000' //development env
-    //baseURL : 'http://yes-florence.herokuapp.com' //production env
+    //baseURL: 'http://localhost:3000' //development env
+    baseURL : 'http://yes-florence.herokuapp.com' //production env
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
